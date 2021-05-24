23e4c416c4afde242c73b7884fa5fc78deec7354
Matt Sanchez <msanchez@tql.com>
2021-05-24 10:53:52 -0400

    Add project files.

---
+++ InterfaceLesson/PitBull.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceLesson
+{
+    class PitBull : IPrint
+    {
+        public String HeadSize { get; set; }
+        public String Type { get; set; } = "Pitbull";
+
+
+        public void Print()
+        {
+            Console.WriteLine($"This dog is a {Type}");
+        }
+    }
+}
---
+++ InterfaceLesson/Shibainu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceLesson
+{
+    class Shibainu : IPrint
+    {
+        public string Characteristic { get; set; } = "Stand-offish";
+        public String Type { get; set; } = "Shibainu";
+
+
+        public void Print()
+        {
+            Console.WriteLine($"This dog is a {Type} and its Stand-offish");
+        }
+    }
+}
